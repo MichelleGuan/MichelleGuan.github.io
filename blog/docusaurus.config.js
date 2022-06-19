5b16f514b18e4226d066fab25b03c7a4e9bd5c1c
Michelle Guan <MichelleGuanHaha@gmail.com>
2022-06-19 16:39:32 +0800

    Update deploy config

--- blog/docusaurus.config.js
+++ blog/docusaurus.config.js
@@ -6,18 +6,18 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: "Michelle's Blog",
+  tagline: "Michelle's Blog",
   url: 'https://michelleguan.github.io',
-  baseUrl: '/',
+  baseUrl: '/blog/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'MichelleGuan', // Usually your GitHub org/user name.
+  projectName: 'MichelleGuan.github.io', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
